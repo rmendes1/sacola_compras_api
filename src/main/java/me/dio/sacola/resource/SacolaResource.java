4835ab5b40181d2ef4098afc9084bdc23f38102d
rmendes1 <joaorenatomendes@gmail.com>
2022-10-04 21:31:12 -0300

    SacolaResource added

--- src/main/java/me/dio/sacola/resource/SacolaResource.java
+++ src/main/java/me/dio/sacola/resource/SacolaResource.java
@@ -1,2 +1,32 @@
-package me.dio.sacola.resource;public class SacolaResource {
+package me.dio.sacola.resource;
+
+import lombok.RequiredArgsConstructor;
+import me.dio.sacola.model.Item;
+import me.dio.sacola.model.Sacola;
+import me.dio.sacola.service.SacolaService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/sacolas")
+public class SacolaResource {
+    private final SacolaService sacolaService;
+
+
+    @PostMapping
+    public Item incluirItemNaSacola(@RequestBody ItemDto itemDto){
+        return sacolaService.incluirItemNaSacola(itemDto);
+    }
+
+    @GetMapping("/{id}")
+    public Sacola verSacola(@PathVariable() Long id){
+        return sacolaService.verSacola(id);
+    }
+
+    @PatchMapping("/fechar-sacola/{sacolaId}")
+    public Sacola fecharSacola(@PathVariable("sacolaId") Long sacolaId,
+                               @RequestParam("formaPagamento") int formaPagamento){
+        return sacolaService.fecharSacola(sacolaId, formaPagamento);
+    }
+
 }
